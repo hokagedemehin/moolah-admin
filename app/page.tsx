608426943b1b543d8427fc4b87028bae3c0e573f
Ibukun <ibk2k7@gmail.com>
2023-08-12 22:26:53 +0100

    fix: removed toploader logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,33 @@
 "use client";
 
-import TopBarProgress from "react-topbar-progress-indicator";
-import { useState } from "react";
+// import TopBarProgress from "react-topbar-progress-indicator";
+// import { useState } from "react";
 import HomePage from "@/screens/HomePage";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function Home() {
-  const [progress, setProgress] = useState(false);
+  // const [progress, setProgress] = useState(false);
 
-  const router = useRouter();
+  // const router = useRouter();
 
-  router.events.on("routeChangeError", () => {
-    console.log("routeChangeError");
-    setProgress(false);
-  });
+  // router.events.on("routeChangeError", () => {
+  //   console.log("routeChangeError");
+  //   setProgress(false);
+  // });
 
-  router.events.on("routeChangeStart", () => {
-    console.log("routeChangeStart");
-    setProgress(true);
-  });
+  // router.events.on("routeChangeStart", () => {
+  //   console.log("routeChangeStart");
+  //   setProgress(true);
+  // });
 
-  router.events.on("routeChangeComplete", () => {
-    console.log("routeChangeComplete");
-    setProgress(false);
-  });
+  // router.events.on("routeChangeComplete", () => {
+  //   console.log("routeChangeComplete");
+  //   setProgress(false);
+  // });
 
   const theme = createTheme({
     breakpoints: {
@@ -46,19 +46,19 @@ export default function Home() {
     },
   });
 
-  TopBarProgress.config({
-    barColors: {
-      "0": "#29d",
-      "0.5": "#29d",
-      "1.0": "#29d",
-    },
-    shadowBlur: 5,
-  });
+  // TopBarProgress.config({
+  //   barColors: {
+  //     "0": "#29d",
+  //     "0.5": "#29d",
+  //     "1.0": "#29d",
+  //   },
+  //   shadowBlur: 5,
+  // });
   const queryClient = new QueryClient();
 
   return (
     <>
-      {progress && <TopBarProgress />}
+      {/* {progress && <TopBarProgress />} */}
       <ThemeProvider theme={theme}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <QueryClientProvider client={queryClient}>
